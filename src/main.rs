fc320505490667717a9cebd96bca4be2a79e6f19
Artur Romanov <arthurslife@ymail.com>
2022-04-12 21:17:35 +0100

    Render sphere at the center of the screen

--- src/main.rs
+++ src/main.rs
@@ -68,8 +68,22 @@ impl Ray {
 }
 
 fn ray_color(ray: Ray) -> Color {
+    if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, &ray) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = ray.dir.unit_vector();
     let t = 0.5 * (unit_direction.y + 1.0);
     // Interpolate from white to blue
     (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
+
+// 𝑡2𝐛⋅𝐛+2𝑡𝐛⋅(𝐀−𝐂)+(𝐀−𝐂)⋅(𝐀−𝐂)−𝑟2=0
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> bool {
+    let oc = ray.orig - *center;
+    let a = ray.dir.dot(ray.dir);
+    let b = 2.0 * oc.dot(ray.dir);
+    let c = oc.dot(oc) - radius*radius;
+    let discriminant = b*b - 4.0*a*c;
+    return discriminant>0.0;
 }
