4c0db79e95a183e98e71aeb5653be5a46ea96cfb
Artur Romanov <arthurslife@ymail.com>
2022-04-16 12:44:18 +0100

    Move camera to a separate struct

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,10 @@ use vector::{Color, Point3, Vec3};
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let image_width = 800;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
 
-    // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let camera = Camera::new();
 
     println!("P3");
     println!("{} {}", image_width, image_height);
@@ -43,20 +34,17 @@ fn main() {
     // Pixels are written from left to right, top to bottom
     let mut row = image_height - 1;
     while row >= 0 {
-        eprintln!("Scanlines remaining: {}", row);
+        // eprintln!("Scanlines remaining: {}", row);
         for col in 0..image_width {
             let u = (col as f64) / (image_width as f64 - 1.0);
             let v = (row as f64) / (image_height as f64 - 1.0);
-            let r = Ray::new(
-                origin,
-                lower_left_corner + u * horizontal + v * vertical - origin,
-            );
+            let r = camera.ray(u, v);
             let pixel_color = ray::ray_color(r, &world);
 
             write_color(pixel_color);
         }
         row -= 1;
-        eprintln!("Done");
+        // eprintln!("Done");
     }
 }
 
@@ -66,4 +54,40 @@ fn write_color(pixel_color: Color) {
     let ib: i32 = (255.999 * pixel_color.z) as i32;
 
     println!("{} {} {}", ir, ig, ib);
+}
+
+struct Camera {
+    origin: Point3,
+    lower_left_corner: Point3,
+    vertical: Vec3,
+    horizontal: Vec3,
+}
+
+impl Camera {
+    fn new() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+
+        let origin = Point3::new(0.0, 0.0, 0.0);
+        let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, viewport_height, 0.0);
+        let lower_left_corner =
+            origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+
+        Camera {
+            origin,
+            lower_left_corner,
+            vertical,
+            horizontal,
+        }
+    }
+
+    fn ray(&self, u: f64, v: f64) -> Ray {
+        Ray::new(
+            self.origin,
+            self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin,
+        )
+    }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,6 +1,14 @@
+// use rand::Rng;
+
 pub static INFINITY: f64 = f64::INFINITY;
 pub static PI: f64 = std::f64::consts::PI;
 
 fn degrees_to_radians(degrees: f64) {
     degrees * PI / 180.0;
 }
+
+// fn random_double() -> f64 {
+//     let mut rng = rand::thread::thread_rng();
+//
+//     rng.gen
+// }
